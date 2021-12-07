a26ce985b7d28262a1a9d7558c160c99b5d18ac5
vitaliyeb <vlvlv.vovov@bk.ru>
2021-12-07 19:36:35 +0500

    done load mock

--- src/jest.test.ts
+++ src/jest.test.ts
@@ -1,16 +1,10 @@
 import {User} from "./api";
-import load from "./load";
-
-jest.mock('./api/load', () => {
-    return {
-        __esModule: true,
-        default: jest.fn(() => 2)
-    };
-});
+import * as testModule from "./api/load";
 
 describe('testing jest api', () => {
     it('mock user getAll ', async function () {
-        jest.spyOn(load, "getAllUsers").mockReturnValue(2);
+        //@ts-ignore
+        jest.spyOn(testModule, "default").mockReturnValue(2);
         expect(User.getAllUsers({ page: 1})).toBe(2);
 
     });
