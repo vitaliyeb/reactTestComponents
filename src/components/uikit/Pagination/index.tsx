6883c1b83263d32e4369dd27419544640b3880db
Vitaliyeb <vlvlv.vovov@bk.ru>
2021-12-13 12:45:53 +0500

    post paginations

--- src/components/uikit/Pagination/index.tsx
+++ src/components/uikit/Pagination/index.tsx
@@ -1,4 +1,5 @@
 import React, {  useEffect, useState } from "react";
+import styles from './style.module.css';
 
 interface IProps {
     Views: (items: any, isLoading: boolean) => JSX.Element;
@@ -20,19 +21,24 @@ export const Pagination = ({ Views, pages, requestElements }: IProps) => {
     }, []);
 
     const startPagesEnumeration =  (s => s <= 0 ? 1 : s + 1)(page - Math.floor(pages / 2));
-    const handlePageClick = (page: number) => {
-
+    const handlePageClick = async (page: number) => {
+        setLoading(true);
+        const { data } = await requestElements(page);
+        setPage(page);
+        setItems(data);
     }
 
     return <div>
         { <Views items={items} isLoading={loading}/> }
-        <div>
+        <div className={styles.pagination}>
             <button>предыдущая</button>
             {
                 Array.from({length: pages}).map((e, i) => {
                     const currentPage = startPagesEnumeration + i;
+                    const isActive = currentPage === page;
 
                     return <button
+                        className={[styles.pageButton, isActive ? styles.active : ''].join(' ')}
                         key={i}
                         onClick={() => handlePageClick(currentPage)}
                     >{ currentPage }</button>
